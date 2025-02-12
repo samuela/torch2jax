61c97911bda7971bf75d04249f0db5b8728cc10c
Samuel Ainsworth <skainsworth@gmail.com>
2025-02-12 01:18:49 -0500

    flake.nix: format

--- flake.nix
+++ flake.nix
@@ -4,19 +4,36 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in with pkgs.python3.pkgs;
-      let jaxlib' = if jaxlib.meta.broken then jaxlib-bin else jaxlib;
-      in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs.python3.pkgs;
+      let
+        jaxlib' = if jaxlib.meta.broken then jaxlib-bin else jaxlib;
+      in
+      {
         defaultPackage = buildPythonPackage {
           pname = "torch2jax";
           version = "0.0.1";
           pyproject = true;
           src = ./.;
-          propagatedBuildInputs = [ jax torch ];
-          nativeCheckInputs = [ jaxlib' pytestCheckHook torchvision ];
+          propagatedBuildInputs = [
+            jax
+            torch
+          ];
+          nativeCheckInputs = [
+            jaxlib'
+            pytestCheckHook
+            torchvision
+          ];
 
           # torchvision downloads models into HOME.
           preCheck = ''
@@ -41,5 +58,6 @@
             twine
           ];
         };
-      });
+      }
+    );
 }
