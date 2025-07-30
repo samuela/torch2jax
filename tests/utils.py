4ea0dbe1ac643a617aae88bf0ee87b3abb0b8a6e
mavenlin <mavenlin@gmail.com>
2025-07-31 07:42:41 +0800

    Use explicit for loop
    Co-authored-by: Samuel Ainsworth <skainsworth@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -43,14 +43,8 @@ def t2j_function_test(f, input_shapes, rng=random.PRNGKey(123), num_tests=5, **a
       # Branching is necessary to avoid `TypeError: iteration over a 0-d array` in the zip.
       if n_inputs > 1:
         argnums = tuple(range(n_inputs))
-        list(
-          map(
-            lambda x: aac(x[0].squeeze(), x[1].squeeze(), **assert_kwargs),
-            zip(
-              grad(t2j(f_), argnums=argnums)(*inputs()),
-              torch.func.grad(f_, argnums=argnums)(*map(j2t, inputs())),
-            ),
-          )
+        for t2j_grad, torch_grad in zip(grad(t2j(f_), argnums=argnums)(*inputs()), torch.func.grad(f_, argnums=argnums)(*map(j2t, inputs()))):
+          aac(t2j_grad.squeeze(), torch_grad.squeeze(), **assert_kwargs)
         )
       else:
         [input] = inputs()
