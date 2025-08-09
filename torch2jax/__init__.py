94b377946ef1e9226edb95ebbbb661ec5d744e24
linmin <mavenlin@gmail.com>
2025-08-09 14:19:56 +0800

    remove out assert for arange

--- torch2jax/__init__.py
+++ torch2jax/__init__.py
@@ -291,7 +291,6 @@ def _assert(condition, message):
 
 @implements(torch.arange, out_kwarg=True)
 def arange(*args, **kwargs):
-  assert kwargs.get("out", None) is None, "TODO: implement `out`"
   dtype = t2j_dtype(
     kwargs.get(
       "dtype",
