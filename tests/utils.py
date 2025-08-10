de1551cd583fd5da9f7981f712c05885eab6608d
linmin <mavenlin@gmail.com>
2025-08-10 21:47:12 +0800

    add tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,12 @@
+from typing import Sequence
+
 import jax
 import jax.numpy as jnp
 import numpy as np
 import torch
 from jax import grad, jit, random
 
-from torch2jax import j2t, t2j
+from torch2jax import FUNCTIONS_WITH_OUT_KWARG, j2t, t2j
 
 
 def aac(tree_a, tree_b, **kwargs):
@@ -29,40 +31,98 @@ def test_t2j_array():
   aac(t2j(torch.eye(3).unsqueeze(0)), jnp.eye(3)[jnp.newaxis, ...])
 
 
+def out_kwarg_test(f, args, kwargs={}, **assert_kwargs):
+  torch_kwargs = jax.tree_util.tree_map(lambda x: j2t(x) if isinstance(x, jnp.dtype) else x, kwargs)
+
+  def torch_function(*torch_args):
+    out1 = f(*torch_args, **torch_kwargs)
+    out2 = torch.zeros_like(out1)
+    out3 = f(*torch_args, out=out2, **torch_kwargs)
+    return out1, out2, out3
+
+  jax_function = t2j(torch_function)
+  jax_out1, jax_out2, jax_out3 = jax_function(*args)
+  assert jax_out2 is jax_out3
+  aac(jax_out1, jax_out3, **assert_kwargs)
+
+
+def Torchish_member_test(f, args, kwargs={}, inplace=False, **assert_kwargs):
+  name = f.__name__ if not inplace else f"{f.__name__}_"
+  torch_kwargs = jax.tree_util.tree_map(lambda x: j2t(x) if isinstance(x, jnp.dtype) else x, kwargs)
+
+  def torch_function(*torch_args):
+    out1 = f(*torch_args, **torch_kwargs)
+    out2 = torch_args[0].clone()
+    out3 = getattr(out2, name)(*torch_args[1:], **torch_kwargs)
+    return out1, out2, out3
+
+  jax_function = t2j(torch_function)
+  jax_out1, jax_out2, jax_out3 = jax_function(*args)
+  if inplace:
+    assert jax_out2 is jax_out3
+  aac(jax_out1, jax_out3, **assert_kwargs)
+
+
+def args_generator(shapes, samplers=None, rng=random.PRNGKey(123)):
+  n_inputs = len(shapes)
+  if samplers is None:
+    samplers = [random.normal] * n_inputs
+  elif not isinstance(samplers, Sequence):
+    samplers = [samplers] * n_inputs
+  while True:
+    rng, rng1 = random.split(rng)
+    args = [sampler(rng, shape=shape) for rng, shape, sampler in zip(random.split(rng1, n_inputs), shapes, samplers)]
+    yield args
+
+
+def forward_test(f, args, kwargs={}, **assert_kwargs):
+  torch_args = jax.tree_util.tree_map(lambda x: j2t(x.copy()) if isinstance(x, (jnp.ndarray, jnp.dtype)) else x, args)
+  torch_kwargs = jax.tree_util.tree_map(lambda x: j2t(x) if isinstance(x, jnp.dtype) else x, kwargs)
+  f_ = lambda *args, **kwargs: f(*args, **torch_kwargs)
+  torch_output = f_(*torch_args)
+  aac(t2j(f_)(*args), torch_output, **assert_kwargs)
+  aac(jit(t2j(f_))(*args), torch_output, **assert_kwargs)
+  return torch_output
+
+
+def backward_test(f, args, kwargs={}, grad_argnums=None, **assert_kwargs):
+  n_inputs = len(args)
+  torch_args = jax.tree_util.tree_map(lambda x: j2t(x.copy()) if isinstance(x, (jnp.ndarray, jnp.dtype)) else x, args)
+  torch_kwargs = jax.tree_util.tree_map(lambda x: j2t(x) if isinstance(x, jnp.dtype) else x, kwargs)
+  # TODO: consider doing this for all functions by doing eg f_ = lambda x: torch.sum(f(x) ** 2)
+  # Can only calculate gradients on scalar-output functions
+  f_ = lambda *args: f(*args, **torch_kwargs).flatten()[0]
+  if grad_argnums is None:  # default, test grad wrt all
+    argnums = tuple(range(n_inputs))
+  elif isinstance(grad_argnums, int):  # test only one input
+    argnums = (grad_argnums,)
+  else:  # user specified tuple
+    assert isinstance(grad_argnums, Sequence)
+    argnums = grad_argnums
+  for t2j_grad, torch_grad in zip(
+    grad(t2j(f_), argnums=argnums)(*args),
+    torch.func.grad(f_, argnums=argnums)(*torch_args),
+  ):
+    aac(t2j_grad.squeeze(), torch_grad.squeeze(), **assert_kwargs)
+
+
 def t2j_function_test(
-  f, input_shapes, samplers=None, rng=random.PRNGKey(123), grad_argnums=None, num_tests=5, **assert_kwargs
+  f, args_shapes, kwargs={}, samplers=None, rng=random.PRNGKey(123), grad_argnums=None, num_tests=5, **assert_kwargs
 ):
-  for test_rng in random.split(rng, num_tests):
-    # This is a thunk since methods like torch.nn.functional.batch_norm mutate the inputs and that affects subsequent
-    # tests. We construct fresh values each time as a mitigation.
-    n_inputs = len(input_shapes)
-    samplers = [random.normal] * n_inputs if samplers is None else samplers
-    inputs = lambda: [
-      sampler(rng, shape=shape) for rng, shape, sampler in zip(random.split(test_rng, n_inputs), input_shapes, samplers)
-    ]
-    torch_output = f(*map(j2t, inputs()))
-    aac(t2j(f)(*inputs()), torch_output, **assert_kwargs)
-    aac(jit(t2j(f))(*inputs()), torch_output, **assert_kwargs)
-
-    # TODO: consider doing this for all functions by doing eg f_ = lambda x: torch.sum(f(x) ** 2)
-    # Can only calculate gradients on scalar-output functions
-    if torch_output.numel() == 1 and len(inputs()) > 0:
-      f_ = lambda *args: f(*args).flatten()[0]
-
-      # Branching is necessary to avoid `TypeError: iteration over a 0-d array` in the zip.
-      if n_inputs > 1:
-        argnums = grad_argnums if grad_argnums is not None else tuple(range(n_inputs))
-        for t2j_grad, torch_grad in zip(
-          grad(t2j(f_), argnums=argnums)(*inputs()), torch.func.grad(f_, argnums=argnums)(*map(j2t, inputs()))
-        ):
-          aac(t2j_grad.squeeze(), torch_grad.squeeze(), **assert_kwargs)
-      else:
-        [input] = inputs()
-        aac(
-          grad(t2j(f_))(input).squeeze(),
-          torch.func.grad(f_)(j2t(input)).squeeze(),
-          **assert_kwargs,
-        )
+  generator = args_generator(args_shapes, samplers, rng)
+  args = next(generator)
+  for i in range(num_tests):
+    args = next(generator)
+    torch_output = forward_test(f, args, kwargs, **assert_kwargs)
+    if isinstance(torch_output, torch.Tensor) and torch_output.numel() == 1 and len(args) > 0:
+      backward_test(f, args, kwargs, grad_argnums=grad_argnums, **assert_kwargs)
+    if f in FUNCTIONS_WITH_OUT_KWARG:
+      out_kwarg_test(f, args, kwargs, **assert_kwargs)
+    if hasattr(f, "__name__"):
+      if hasattr(torch.Tensor, f.__name__):
+        Torchish_member_test(f, args, kwargs, **assert_kwargs)
+      if hasattr(torch.Tensor, f"{f.__name__}_"):
+        Torchish_member_test(f, args, kwargs, inplace=True, **assert_kwargs)
 
 
 def assert_state_dicts_allclose(actual, desired, **kwargs):
