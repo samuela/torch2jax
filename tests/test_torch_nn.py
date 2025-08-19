fix torch.nn.batch_norm gradient error

tests/test_torch_nn.py
@@ -340,7 +340,7 @@ def test_torch_nn_functional_batch_norm():
     # Variance needs to be positive for sensible results so we square it.
     running_var = running_var.pow(2)
     out = torch.nn.functional.batch_norm(input, running_mean, running_var, weight=weight, bias=bias, training=True)
-    return torch.cat([out.flatten(), running_mean.flatten(), running_var.flatten()])
+    return out, running_mean, running_var
 
   t2j_function_test(f, [(2, 3), (3,), (3,), (3,), (3,)], atol=1e-5, tests=tests)
   t2j_function_test(f, [(2, 3, 5), (3,), (3,), (3,), (3,)], atol=1e-6, tests=tests)

torch2jax/__init__.py
@@ -595,8 +595,8 @@ def batch_norm(input, running_mean, running_var, weight=None, bias=None, trainin
 
     mean = jnp.mean(x_, axis=(0, 2))
     var = jnp.var(x_, axis=(0, 2), ddof=1)
-    running_mean.value = momentum * mean + (1 - momentum) * running_mean.value
-    running_var.value = momentum * var + (1 - momentum) * running_var.value
+    running_mean.value = jax.lax.stop_gradient(momentum * mean + (1 - momentum) * running_mean.value)
+    running_var.value = jax.lax.stop_gradient(momentum * var + (1 - momentum) * running_var.value)
 
     # Why different ddof values are used for running_var and working_var I will never understand...
     working_mean = mean